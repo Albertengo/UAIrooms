82baff3843315927321171fad0be2680314fc1fa
Albertengo <145947798+Albertengo@users.noreply.github.com>
2025-04-24 01:24:01 -0300

    luchando con el mov de cámara
    cámara funciona, ya se puede mover pero el player no se mueve hacia la dirección q mira

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //NOTA: intenté hacer el codigo sin seguir mucho el video q mandé x el canal de programación y no me funcionó,
+    // capaz tendriamos q ver de hacerlo más parecido o ver otra manera?? es medio tarde asi q lo dejo acá je -ori
 
     PlayerInput playerInput;
     InputAction moveAction;
-    // Start is called before the first frame update
+    [SerializeField] float Speed;
+    [SerializeField] Transform cameraTransform;
+
     void Start()
     {
        playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
     void Update()
     {
         MovePlayer();
@@ -25,6 +28,12 @@ public class PlayerMovement : MonoBehaviour
     {
        Vector2 direction = moveAction.ReadValue<Vector2>();
         transform.position += new Vector3(direction.x, 0, direction.y) * Time.deltaTime;
-
+        
+        /* este codigo hizo q el player como q SALTE a una posicion especifica, es re raro...
+        Vector3 move = new Vector3(direction.x, 0f, direction.y);
+        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+        move.y = 0f;
+        transform.position = move * Speed; //Time.deltaTime * Speed;
+        */
     }
 }
