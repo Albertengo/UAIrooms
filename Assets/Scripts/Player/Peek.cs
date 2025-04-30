8f9f53df12a6bffd44503b041b6582ae05c039f6
Albertengo <145947798+Albertengo@users.noreply.github.com>
2025-04-30 03:00:01 -0300

    yarn spinner omg
    para la narrativa y opciones

--- Assets/Scripts/Player/Peek.cs
+++ Assets/Scripts/Player/Peek.cs
@@ -6,7 +6,7 @@ public class Peek : MonoBehaviour
 {
     //Logicamente lo q se puede hacer para esta mecánica es que si se aprieta Q/E se reproduzca una animacion de la cámara
     //que solo se reproduzca cuando estás quieto, si no, no (para evitar bugs)
-
+    
     // Start is called before the first frame update
     void Start()
     {
